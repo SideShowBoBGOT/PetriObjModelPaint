07e181710eff5b734588d9811aad6aed34e8c5ff
account <account@mail.com>
2025-01-12 17:12:17 +0200

    remove CreateNet and CreateNetUntitled from LibNet

--- petri-obj-paint/src/main/java/ua/stetsenkoinna/LibNet/NetLibrary.java
+++ petri-obj-paint/src/main/java/ua/stetsenkoinna/LibNet/NetLibrary.java
@@ -460,35 +460,6 @@ public class NetLibrary {
     }
 
     @NetLibraryMethod
-    public static PetriNet CreateNet() throws ExceptionInvalidNetStructure, ExceptionInvalidTimeDelay {
-        ArrayList<PetriP> d_P = new ArrayList<>();
-        ArrayList<PetriT> d_T = new ArrayList<>();
-        ArrayList<ArcIn> d_In = new ArrayList<>();
-        ArrayList<ArcOut> d_Out = new ArrayList<>();
-        PetriNet d_Net = new PetriNet("", d_P, d_T, d_In, d_Out);
-        PetriP.initNext();
-        PetriT.initNext();
-        ArcIn.initNext();
-        ArcOut.initNext();
-        return d_Net;
-    }
-
-    @NetLibraryMethod
-    public static PetriNet CreateNetUntitled() throws ExceptionInvalidNetStructure, ExceptionInvalidTimeDelay {
-        ArrayList<PetriP> d_P = new ArrayList<>();
-        ArrayList<PetriT> d_T = new ArrayList<>();
-        ArrayList<ArcIn> d_In = new ArrayList<>();
-        ArrayList<ArcOut> d_Out = new ArrayList<>();
-        d_P.add(new PetriP("P1", 0));
-        PetriNet d_Net = new PetriNet("Untitled", d_P, d_T, d_In, d_Out);
-        PetriP.initNext();
-        PetriT.initNext();
-        ArcIn.initNext();
-        ArcOut.initNext();
-        return d_Net;
-    }
-
-    @NetLibraryMethod
     public static PetriNet CreateNetUntitledHappy() throws ExceptionInvalidNetStructure, ExceptionInvalidTimeDelay {
         ArrayList<PetriP> d_P = new ArrayList<>();
         ArrayList<PetriT> d_T = new ArrayList<>();
